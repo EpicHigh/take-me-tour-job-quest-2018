8c2a9b1e9ef215565997e4b4280ef1ca2a02a401
EpicHigh <prasit_tongpradit@windowslive.com>
2019-02-20 01:09:12 +0700

    Updates

---
+++ precache-manifest.7e4f25642549f78f3ce193a199982215.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6600fe839ad685d9ac82",
+    "url": "/static/js/main.6600fe83.chunk.js"
+  },
+  {
+    "revision": "0a801a43d4cb61bf8439",
+    "url": "/static/js/2.0a801a43.chunk.js"
+  },
+  {
+    "revision": "6600fe839ad685d9ac82",
+    "url": "/static/css/main.037e6c53.chunk.css"
+  },
+  {
+    "revision": "41978d922fd0c2388c50958a22555881",
+    "url": "/index.html"
+  }
+];
